411af85740260d71e879ab97c7e501a3b85e3608
unknown <josecosta0025@digitalcollege.internal>
2025-05-28 11:47:12 -0300

    components

---
+++ src/pages/jair/index.jsx
@@ -0,0 +1,8 @@
+const jair = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default jair;
--- src/routes/Paths/index.jsx
+++ src/routes/Paths/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SiteLayout from "../../SiteLayout";
 import Hideki from "../../pages/Hideki";
+import Jair from "../../pages/jair";
 
 const Paths = () => {
     return (
@@ -9,6 +10,7 @@ const Paths = () => {
             <Routes>
                 <Route path="/" element={<SiteLayout />}>
                     <Route path="/hideki" element={<Hideki />} />
+                     <Route path="/jair" element={<Jair />} />
                 </Route>
             </Routes>
         </BrowserRouter>
