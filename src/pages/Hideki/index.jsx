25306aa0a2fb8f314e6fef785711a4f834f06b72
Cesar Hideki <cesar.nagano1@gmail.com>
2025-05-28 09:12:32 -0300

    criacao do projeto

---
+++ src/pages/Hideki/index.jsx
@@ -0,0 +1,8 @@
+const Hideki = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default Hideki;
---
+++ src/pages/Padrao/index.jsx
@@ -0,0 +1,8 @@
+const Padrao = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default Padrao;
