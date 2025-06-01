fd5a1bd4dcc9954942de1ee69110e25b1941cb7a
GabriellaScdev <gabriellasc.ce@gmail.com>
2025-06-01 20:31:51 -0300

    imagem, titulo e background adicionados topo da pagina

--- src/pages/Gabriella/ComponentsGabi/Card/index.jsx
+++ src/pages/Gabriella/ComponentsGabi/Card/index.jsx
@@ -1,6 +1,7 @@
 export default function CardGabi({ nome , numero , instagram}) {
   return (
-    <div className="bg-blue-50 p-4 rounded shadow">
+    
+    <div className=" bg-blue-50 p-4 rounded shadow">
       <p className="font-medium">{nome}</p>
       <p className="text-sm text-gray-700">📞 {numero}</p>
       <p className="text-sm text-blue-600">📸 @{instagram}</p>
--- src/pages/Gabriella/ComponentsGabi/Title/index.jsx
+++ src/pages/Gabriella/ComponentsGabi/Title/index.jsx
@@ -1,8 +1,23 @@
+
+import contatos from '../assetsgabi/contatos.png';
+
 const Title = ({ titulo }) => {
     return ( 
-        <h1 className="text-center p-3 font-extrabold">
-            {titulo}
-        </h1>
+         <header className="w-screen fixed top-0 left-0 z-50 bg-gradient-to-r from-blue-200 to-blue-100 py-2 px-6 shadow-md">
+            <div className="flex items-center">
+                {/* Imagem */}
+                <img 
+                    src={contatos} 
+                    alt="Logo da página" 
+                    className="w-16 h-16 rounded-full object-cover"
+                />
+
+                {/* Título */}
+                <h1 className="ml-4 text-2xl font-extrabold text-gray-800">
+                    Contatinhos ... {titulo}
+                </h1>
+            </div>
+        </header>
      );
 }
  
